11fc8f7443090681e5047227d990248fcd93e5dc
Vinicius Perrone <perronevinicius2018@gmail.com>
2022-07-20 13:03:00 -0300

    chore: added svg config

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,17 @@
-import { VStack } from 'native-base';
+import { Heading, VStack } from "native-base";
 
-function SignIn(){
-  return(
-    <VStack flex={1} alignItems="center">
+import Logo from "../assets/logo_primary.svg";
 
+function SignIn() {
+  return (
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
+
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acessar sua conta
+      </Heading>
     </VStack>
-  )
+  );
 }
 
 export default SignIn;

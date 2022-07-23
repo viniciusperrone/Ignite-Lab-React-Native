93b008cd305d4a5023d7f5048b299d60f68b223e
Vinicius Perrone <perronevinicius2018@gmail.com>
2022-07-23 19:58:14 -0300

    chore: route management

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,10 +1,30 @@
+import { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
+
 import { AppRoutes } from "./app.routes";
+import { SignIn } from "@screens/SignIn";
+import { Loading } from "@components/Loading";
 
 export function Routes() {
+  const [loading, setIsLoading] = useState<boolean>(true);
+  const [user, setUser] = useState<FirebaseAuthTypes.User>();
+
+  useEffect(() => {
+    const subscribe = auth().onAuthStateChanged((response) => {
+      setUser(response);
+      setIsLoading(false);
+    });
+
+    return subscribe;
+  }, []);
+
+  if (loading) {
+    return <Loading />;
+  }
   return (
     <NavigationContainer>
-      <AppRoutes />
+      {user ? <AppRoutes /> : <SignIn />}
     </NavigationContainer>
   );
 }

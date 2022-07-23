8331ff0c205e9e93629db2a800c2c738b7106ff6
Vinicius Perrone <perronevinicius2018@gmail.com>
2022-07-23 19:57:11 -0300

    feat: user authentication

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import { Alert } from "react-native";
 import { Heading, Icon, useTheme, VStack } from "native-base";
+import auth from "@react-native-firebase/auth";
 import { Envelope, Key } from "phosphor-react-native";
 
 import { Input } from "@components/Input";
@@ -8,12 +10,44 @@ import { Button } from "@components/Button";
 import { IUserLogin } from "@shared/interfaces";
 import Logo from "assets/logo_primary.svg";
 
-function SignIn() {
+export function SignIn() {
   const [user, setUser] = useState<IUserLogin>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const { colors } = useTheme();
 
   function onChange(value: string, name: string) {
     setUser({ [name]: value, ...user });
+  }
+
+  function handleSignIn() {
+    const { email, password } = user;
+
+    if (!email || !password) {
+      return Alert.alert("Entrar", "Informe e-mail e senha.");
+    }
+
+    setIsLoading(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => {
+        console.log(error);
+        if (error.code === "auth/invalid-email") {
+          return Alert.alert("Entrar", "E-mail inválido");
+        }
+
+        if (
+          error.code === "auth/user-not-found" ||
+          error.code === "auth/wrong-password"
+        ) {
+          return Alert.alert("Entrar", "E-mail ou senha inválido");
+        }
+
+        return Alert.alert("Entrar", "Não foi possível acessar");
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   }
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -29,6 +63,7 @@ function SignIn() {
         InputLeftElement={
           <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
         }
+        value={user.email}
         onChangeText={(e) => onChange(e, "email")}
       />
 
@@ -37,11 +72,15 @@ function SignIn() {
         placeholder="Senha"
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
         secureTextEntry
+        value={user.password}
         onChangeText={(e) => onChange(e, "password")}
       />
-      <Button title="Entrar" w="full" />
+      <Button
+        title="Entrar"
+        w="full"
+        onPress={handleSignIn}
+        isLoading={isLoading}
+      />
     </VStack>
   );
 }
-
-export default SignIn;

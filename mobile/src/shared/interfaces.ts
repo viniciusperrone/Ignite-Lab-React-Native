04f1ff3355784962400c4001810f51eb494a8af1
Vinicius Perrone <perronevinicius2018@gmail.com>
2022-07-24 19:03:52 -0300

    feat: close request

--- mobile/src/shared/interfaces.ts
+++ mobile/src/shared/interfaces.ts
@@ -20,6 +20,12 @@ export interface IOrderResponse {
   created_at: FirebaseFirestoreTypes.Timestamp;
 }
 
+export interface IOrderDetails extends IOrder {
+  description: string;
+  solution: string;
+  closed: string;
+}
+
 export interface IRequest {
   patrimony: string;
   description: string;
--- mobile/src/shared/types.ts
+++ mobile/src/shared/types.ts
@@ -1,6 +1,16 @@
 import { IPressableProps } from "native-base";
+import { FirebaseFirestoreTypes } from "@react-native-firebase/firestore";
 import { IOrder } from "./interfaces";
 
 export type OrderProps = IPressableProps & {
   data: IOrder;
+};
+
+export type OrderFirestore = {
+  patrimony: string;
+  description: string;
+  status: "open" | "closed";
+  solution?: string;
+  created_at: FirebaseFirestoreTypes.Timestamp;
+  closed_at?: FirebaseFirestoreTypes.Timestamp;
 };
